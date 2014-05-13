ec1cd1d17734374888586c6b069662f7ec99266f
Sebastian Heil <mail+git@sebastianheil.de>
2014-05-14 01:11:36 +0200

    [emacs] change windmove keybinding meta key from shift to C-c

--- emacs/.emacs.d/config/init-kbd-shortcuts.el
+++ emacs/.emacs.d/config/init-kbd-shortcuts.el
@@ -11,8 +11,12 @@
 (define-key global-map (kbd "C-}") 'enlarge-window-horizontally)
 (define-key global-map (kbd "C-^") 'enlarge-window)
 
-;; Navigating: Windmove uses Shift-<up> etc.
-(windmove-default-keybindings)
-
+;; Navigating: Windmove uses Shift-<up>, etc. by default
+;(windmove-default-keybindings)
+; change it to C-c-<up>, etc.
+(global-set-key (kbd "C-c <left>")  'windmove-left)
+(global-set-key (kbd "C-c <right>") 'windmove-right)
+(global-set-key (kbd "C-c <up>")    'windmove-up)
+(global-set-key (kbd "C-c <down>")  'windmove-down)
 
 (provide 'init-kbd-shortcuts)
