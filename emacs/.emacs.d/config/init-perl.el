9e76db281baa49f3457a4b353cb206a716dd9448
Sebastian Heil <mail+git@sebastianheil.de>
2014-05-14 01:16:49 +0200

    [emacs] removed all cperl specific settings. Will be enabled later on demand.

--- emacs/.emacs.d/config/init-perl.el
+++ emacs/.emacs.d/config/init-perl.el
@@ -4,33 +4,34 @@
 
 ;;; some default values for cperl. adapted from prelude.
 (defun some-cperl-mode-defaults ()
-  (setq cperl-indent-level 4)
-  (setq cperl-continued-statement-offset 8)
-  ;; cperl-hairy affects all those variables, but I prefer
-  ;; a more fine-grained approach as far as they are concerned
-  (setq cperl-font-lock t)
-  (setq cperl-electric-lbrace-space t)
-  (setq cperl-electric-parens nil)
-  (setq cperl-electric-linefeed nil)
-  ;; expand perl keywords (like foreach, while, ...)
-  (setq cperl-electric-keywords t)
-  (setq cperl-info-on-command-no-prompt t)
-  (setq cperl-clobber-lisp-bindings t)
-  ;; should newline after parenthesis be indented (nil) or not (t)
-  (setq cperl-indent-parens-as-block t)
-  ;; zeige cperl help nach einer gewissen zeit
-  (setq cperl-lazy-help-time 3)
-
-  ;; if you want all the bells and whistles
-  (setq cperl-hairy t)
-
-  ;; to make cperl always highlight scalar variables
-  (setq cperl-highlight-variables-indiscriminately t)
-
-  ;; array and hash syntax highlighting default is to bright therefore we change it
-;  (set-face-background 'cperl-array-face nil)
-;  (set-face-foreground 'cperl-array-face "blue")
-;  (set-face-background 'cperl-hash-face nil)
+;  (setq cperl-hairy t)
+;  ;; affects `cperl-font-lock',`cperl-electric-lbrace-space',`cperl-electric-parens', `cperl-electric-linefeed', `cperl-electric-keywords', `cperl-info-on-command-no-prompt', `cperl-clobber-lisp-bindings', `cperl-lazy-help-time'
+;  ;; but lets make it explict
+;
+;  (setq cperl-indent-level 4)
+;  ;; insert automatically newline before and after braces
+;;  (setq cperl-auto-newline t)
+;  (setq cperl-continued-statement-offset 8)
+;  ;; cperl-hairy affects all those variables, but I prefer
+;  ;; a more fine-grained approach as far as they are concerned
+;  (setq cperl-font-lock t)
+;  (setq cperl-electric-lbrace-space t)
+;  (setq cperl-electric-parens t)
+;  (setq cperl-electric-linefeed nil)
+;  ;; expand perl keywords (like foreach, while, ...)
+;  (setq cperl-electric-keywords t)
+;  (setq cperl-info-on-command-no-prompt t)
+;  (setq cperl-clobber-lisp-bindings t)
+;  ;; zeige cperl help nach einer gewissen zeit
+;  (setq cperl-lazy-help-time 3)
+;
+;  ;; to make cperl always highlight scalar variables
+;  (setq cperl-highlight-variables-indiscriminately t)
+;
+;  ;; array and hash syntax highlighting default is to bright therefore we change it
+;;  (set-face-background 'cperl-array-face nil)
+;;  (set-face-foreground 'cperl-array-face "blue")
+;;  (set-face-background 'cperl-hash-face nil)
   ;; disable higlighting trailing whitespaces (with underscore)
   (setq cperl-invalid-face nil)
   (font-lock-comment-annotations))
