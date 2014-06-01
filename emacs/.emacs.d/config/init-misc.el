77c692ff379bea9202477b8a748d4c32be54b026
Sebastian Heil <mail+git@sebastianheil.de>
2014-06-01 13:31:18 +0200

    [emacs] added multiple-cursors plugin

--- emacs/.emacs.d/config/init-misc.el
+++ emacs/.emacs.d/config/init-misc.el
@@ -11,4 +11,13 @@
 ;; clean up obsolete buffers automatically
 (require 'midnight)
 
+(add-hook 'after-init-hook 'misc-after-init-hook)
+(defun misc-after-init-hook ()
+  (require 'multiple-cursors)
+  (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+  (global-set-key (kbd "C->") 'mc/mark-next-like-this)
+  (global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+  (global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+)
+
 (provide 'init-misc)
