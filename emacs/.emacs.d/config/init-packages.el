633caa783cefc9b3666a2eaeea4cc5ac419c4efa
Sebastian Heil <mail+git@sebastianheil.de>
2014-06-04 16:12:45 +0200

    [emacs] added automatic installation of missing packages

--- emacs/.emacs.d/config/init-packages.el
+++ emacs/.emacs.d/config/init-packages.el
@@ -9,4 +9,35 @@
 ;(package-initialize)
 ;(setq package-enable-at-startup nil)
 
+; shamelessly stolen from: http://stackoverflow.com/a/10102154/1128373
+(package-initialize)
+;(setq url-httqp-attempt-keepalives nil)
+
+(defvar init-packages
+  '(dired+ helm midnight multiple-cursors)
+  "A list of packages to ensure are installed at launch.")
+
+(defun is-package-installed (p)
+  "Ensure PACKAGE is installed."
+  (cond ((package-installed-p p) t)
+     (t nil)))
+
+(defun installed-packages ()
+  "Ensure all PACKAGES are installed."
+  (mapcar 'is-package-installed init-packages))
+
+(defun install-missing-packages ()
+  (interactive)
+  (unless (every 'identity (installed-packages))
+    ;; check for new packages
+    (message "%s" "Emacs is now refreshing its package database...")
+    (package-refresh-contents)
+    (message "%s" " done. ")
+    ;; install the missing packages
+    (dolist (package init-packages)
+      (when (not (package-installed-p package))
+        (package-install package)))))
+
+(install-missing-packages)
+
 (provide 'init-packages)
