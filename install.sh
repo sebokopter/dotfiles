14a6f847ce90f2bba295ee0c126b0a8a256c452d
Sebastian Heil <mail+git@sebastianheil.de>
2015-02-02 16:49:14 +0100

    [install.sh] make install.sh overwrite existing symlinks

--- install.sh
+++ install.sh
@@ -7,6 +7,6 @@ for dir in $( find -mindepth 1 -maxdepth 1 ! -name ".git*" -type d ); do
         if [[ -e $dst ]]; then
             mv -f $dst $dst.backup
         fi
-        ln -s $src $dst
+        ln -sf $src $dst
     done
 done
